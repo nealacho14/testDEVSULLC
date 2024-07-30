a64fe1cab19983c6867ceb1ea1b685cfe535b83c
Neal <nealdeoroing@gmail.com>
2024-07-29 22:09:49 -0500

    Agregar id Dashboard

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   reporter: 'mochawesome',
+  projectId: "5w4b59",
     e2e: {
       baseUrl: 'https://petstore.swagger.io/v2',
       screenshotOnRunFailure: true,
